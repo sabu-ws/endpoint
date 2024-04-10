ed0173bcb6428335debe8c38040d0771d198de73
LooXzy <looxzy@protonmail.com>
2024-04-10 17:47:38 +0200

    add browser front

--- app/blueprints/browser/browser_bp.py
+++ app/blueprints/browser/browser_bp.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
 
 browser_bp = Blueprint("browser", __name__, template_folder="templates")
 
 @browser_bp.route("/")
 def index():
-	return "browser page"
+	return redirect("/browser/usb")
+
+@browser_bp.route("/usb")
+def browserUsb():
+	return render_template("browser_usb.html")
+
+@browser_bp.route("/server")
+def browserServer():
+	return render_template("browser_server.html")
