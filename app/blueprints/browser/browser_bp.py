6fd44da8e76d1a9a8e3e8b7fd7a23402d44948e3
LooXzy <looxzy@protonmail.com>
2024-04-15 10:22:38 +0200

    browser usb

--- app/blueprints/browser/browser_bp.py
+++ app/blueprints/browser/browser_bp.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for
+import os
 
 from app.utils.api.function import login_required
 
@@ -7,14 +8,30 @@ browser_bp = Blueprint("browser", __name__, template_folder="templates")
 @browser_bp.route("/")
 @login_required
 def index():
-	return redirect(url_for('browser.browserUsb'))
+	return redirect(url_for('browser.usb_path'))
 
-@browser_bp.route("/usb")
+@browser_bp.route("/usb/path/<path:MasterListDir>")
+@browser_bp.route("/usb/path/")
 @login_required
-def browserUsb():
-	return render_template("browser_usb.html")
+def usb_path(MasterListDir=""):
+	path_temp_usb = "/mnt"
+	joining = os.path.join(path_temp_usb, MasterListDir)
+	cur_dir = MasterListDir
+	if not os.path.exists(joining):
+		abort(404)
+	if os.path.isdir(joining):
+		new_path = os.listdir(joining)
+		list_items = [i for i in os.walk(joining)][0]
+		items_dir = list_items[1]
+		items_file = list_items[2]
+	return render_template("browser_usb.html", items_file=items_file, items_dir=items_dir, cur_dir=cur_dir)
+
+@browser_bp.route("/usb/delete")
+@login_required
+def usb_delete():
+	return redirect(url_for('browser.usb_path'))
 
 @browser_bp.route("/server")
 @login_required
-def browserServer():
+def server_path():
 	return render_template("browser_server.html")
---
+++ app/static/js/scripts/browser_usb.js
@@ -0,0 +1,26 @@
+$(".onclick_object").click(function(){
+	var href = $(this).closest("tr").find(".location").closest("a").attr("href")
+	if(href) {
+		window.location = href;
+	}
+});
+
+// Return Button
+$("#returnButton").click(function(){
+	var url = window.parent.location.href;
+	if(url.substr(url.length - 2) == "/"){
+		url=url.slice(0,-1)
+	}
+	if(url.slice(url.lastIndexOf('/')) != '/browser/usb/path/'){
+		var to = url.lastIndexOf('/');
+		to = to == -1 ? url.length : to + 1;
+		url = url.substring(0, to -1 );
+		window.location=url;
+	}
+});
+
+$(function(){
+	if(window.location.pathname != "/browser/usb/path/"){
+		$("#returnButton").removeAttr('hidden');
+	}
+});
