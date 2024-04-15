c65f297cff6c486928f8694cdbce5e77e5060ee5
LooXzy <looxzy@protonmail.com>
2024-04-15 10:44:42 +0200

    update front

--- app/blueprints/scan/scan_bp.py
+++ app/blueprints/scan/scan_bp.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 scan_bp = Blueprint("scan", __name__, template_folder="templates")
 
 @scan_bp.route("/")
 def index():
-	return "scan page"
+	return render_template("scan.html")
